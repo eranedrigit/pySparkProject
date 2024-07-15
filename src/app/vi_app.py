798b6789ac8bb71d6a1b015881e552b0606c519a
Eran Edri <eran.edri@pagaya.com>
2024-07-15 14:28:33 +0300

    udpate project structure

---
+++ src/app/vi_app.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession, DataFrame
+
+
+class App:
+    def __init__(self):
+        print("Building spark session...")
+        self.spark = SparkSession.builder.appName("ViApp").getOrCreate()
+        print("Spark session built!")
+
+    def load_data(self, input_path) -> DataFrame:
+        print("Loading data...")
+        return self.spark.read.csv(input_path, header=True, inferSchema=True)
+
+    def stop(self):
+        self.spark.stop()
